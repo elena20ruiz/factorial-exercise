c408b9c738e6bfcff8ece657dcee65a5e7a18a61
Elena Ruiz <elena.ruiz.bdn@gmail.com>
2020-03-24 16:28:12 +0100

    Show errors on add/edit event

--- client/src/components/Modal/ModalEvent.js
+++ client/src/components/Modal/ModalEvent.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { DialogContent , DialogActions, TextField, Dialog, DialogTitle, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { DialogContent, DialogActions, TextField, Dialog, DialogTitle, Button } from '@material-ui/core';
 import ApiFactory from '../../api/ApiFactory';
 
+const useStyles = makeStyles(theme => ({
+    field: {
+        marginTop: '1em'
+    },
+    errorMsg : {
+        color: 'white',
+        display: 'block',
+        backgroundColor: theme.palette.error.light,
+        padding: '0.5em',
+        borderRadius: '5px'
+    }
+}));
 
 function ModalEvent(props) {
+
+    const classes = useStyles();
+
     const [element, setElement] = React.useState([]);
+    const [error, setError] = React.useState(false);
+
 
     React.useEffect(() => {
         // Format dates
@@ -38,7 +56,7 @@ function ModalEvent(props) {
     function handleChange(e) {
         var id = e.target.id;
         var value = e.target.value;
-        setElement({...element, [id]: value})
+        setElement({ ...element, [id]: value })
     }
 
 
@@ -47,17 +65,34 @@ function ModalEvent(props) {
     }
 
     function handleSave() {
+
+        // Check input: 
+        if (!element.title || !element.initDate || !element.endDate) {
+            setError('Title, Init Date and End Date are required');
+            return;
+        }
+
+        // Check dates
+        var d1 = new Date(element.initDate);
+        var d2 = new Date(element.endDate);
+        if (d1 > d2) {
+            setError('Init day has to be lower than end date');
+            return;
+        }
+
         var f;
         if (props.action == 'add') f = 'addEvent';
         else f = 'updateEvent';
-        
+
         const actionEvent = ApiFactory.get(f);
         actionEvent(element)
-            .then((res)=> {
+            .then(() => {
+                setError(false);
                 props.onClose();
-            })  
-            .catch((res)=> {
+            })
+            .catch((res) => {
                 // Show error
+                setError(res);
             });
     }
 
@@ -65,53 +100,74 @@ function ModalEvent(props) {
         <Dialog open={true} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">Event</DialogTitle>
             <DialogContent>
-                <TextField
-                    id="title"
-                    label="Title"
-                    type="text"
-                    onChange={handleChange}
-                    value={element['title']}
+                {
+                    error &&
+                    <label className={classes.errorMsg}>
+                        {error}
+                    </label>
+                }
+                <form>
+                    <TextField
+                        id="title"
+                        label="Title"
+                        type="text"
+                        onChange={handleChange}
+                        value={element['title']}
 
-                    fullWidth
-                    variant="outlined"
-                />
-                <TextField
-                    id="initDate"
-                    label="Init date"
-                    type="date"
-                    onChange={handleChange}
-                    value={element['initDate']}
-
-                    fullWidth
-                    variant="outlined"
-                />
-                <TextField
-                    id="endDate"
-                    label="End Date"
-                    type="date"
-                    onChange={handleChange}
-                    value={element['endDate']}
-
-                    fullWidth
-                    variant="outlined"
-                />
-                <TextField
-                    id="description"
-                    label="Description"
-                    type="text"
-                    onChange={handleChange}
-                    value={element['description']}
-
-                    fullWidth
-                    variant="outlined"
-                />
+                        fullWidth
+                        variant="outlined"
+                        className={classes.field}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        id="initDate"
+                        label="Init date"
+                        type="date"
+                        onChange={handleChange}
+                        value={element['initDate']}
+                        fullWidth
+                        variant="outlined"
+                        className={classes.field}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        id="endDate"
+                        label="End Date"
+                        type="date"
+                        onChange={handleChange}
+                        value={element['endDate']}
+                        fullWidth
+                        variant="outlined"
+                        className={classes.field}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        id="description"
+                        label="Description"
+                        type="text"
+                        onChange={handleChange}
+                        value={element['description']}
+                        fullWidth
+                        variant="outlined"
+                        className={classes.field}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </form>
             </DialogContent>
             <DialogActions>
-            <Button onClick={handleCancel} color="primary">
-                Cancel
+                <Button onClick={handleCancel} color="primary">
+                    Cancel
             </Button>
-            <Button onClick={handleSave} color="primary">
-                Save
+                <Button onClick={handleSave} color="primary">
+                    Save
             </Button>
             </DialogActions>
         </Dialog>
